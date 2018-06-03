a7d0894f34a777b85f3699d3c891b611694da788
Kris Pinter <pinter.krisztian89@gmail.com>
2018-06-03 20:23:37 +0100

    song detail

--- Lyrics-App/client/components/SongDetail.js
+++ Lyrics-App/client/components/SongDetail.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import gql from 'graphql-tag';
-import {graphql} from 'graphql-apollo';
+import {graphql} from 'react-apollo';
+import {Link} from 'react-router';
+import fetchSong from '../queries/fetchSong';
 
 class SongDetail extends React.Component {
-    render(){
-        return(
+    render(){        
+        const { song } = this.props.data;
+ 
+        if (!song) {
+            return <div>Loading...</div>
+        }
+
+        return (
             <div>
-                <h3>Song detail...</h3>
+                <Link to="/">Back</Link>
+                <h3>{song.title}</h3>
             </div>
         )
     }
 }
 
-export default SongDetail;
+export default graphql(fetchSong, {
+    options: (props) => { return {variables: { id: props.params.id}}}
+})(SongDetail);
